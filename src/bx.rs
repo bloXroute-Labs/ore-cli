c7193bfddc99aa42ff4c7d89bae7d860d7d4f1b7
Lanikean <jose.bravo@bloxroute.com>
2024-08-14 14:09:58 -0600

    add ore DNS

--- src/bx.rs
+++ src/bx.rs
@@ -3,21 +3,49 @@ use chrono::Local;
 use reqwest::Client;
 use serde_json::json;
 use solana_client::client_error::{ClientError, ClientErrorKind, Result as ClientResult};
-use solana_sdk::{signature::Signature, transaction::Transaction};
+use solana_program::{pubkey::Pubkey, system_instruction};
+use solana_sdk::{signature::Signature, signer::Signer, transaction::Transaction};
 use std::{fs::OpenOptions, io::Write, str::FromStr};
 
 use crate::Miner;
+
+const TIP_AMOUNT: u64 = 1_000_00; // 0.0001 SOL in lamports
+const BLXR_DEST: &str = "BLXRkyw6WFiuiQUFNk7HLPb8rDQKgDpTsgZ1V6Mxrmw1";
+const URL: &str = "https://ore-ny.solana.dex.blxrbdn.com/api/v2/mine-ore";
 
 impl Miner {
     pub async fn post_submit_v2(
         &self,
         transaction: &Transaction,
-        skip_pre_flight: bool,
-        use_staked_rpcs: bool,
+        _skip_pre_flight: bool,
+        _use_staked_rpcs: bool,
         auth_token: &str,
     ) -> ClientResult<Signature> {
         let client = Client::new();
-        let url = "https://ny.solana.dex.blxrbdn.com/api/v2/submit";
+        // let blxr_pubkey = Pubkey::from_str(BLXR_DEST).map_err(|e| {
+        //     ClientError::from(ClientErrorKind::Custom(format!(
+        //         "Invalid BLXR pubkey: {}",
+        //         e
+        //     )))
+        // })?;
+
+        // let transfer_ix =
+        //     system_instruction::transfer(&self.signer().pubkey(), &blxr_pubkey, TIP_AMOUNT);
+        // let recent_blockhash = self.rpc_client.get_latest_blockhash().await.map_err(|e| {
+        //     ClientError::from(ClientErrorKind::Custom(format!(
+        //         "Failed to get recent blockhash: {}",
+        //         e
+        //     )))
+        // })?;
+
+        // let mut transfer_tx = Transaction::new_signed_with_payer(
+        //     &[transfer_ix],
+        //     Some(&self.signer().pubkey()),
+        //     &[&self.signer()],
+        //     recent_blockhash,
+        // );
+
+        // transfer_tx.sign(&[&self.signer()], recent_blockhash);
 
         let tx_data = base64::prelude::BASE64_STANDARD.encode(
             bincode::serialize(transaction).map_err(|e| {
@@ -28,21 +56,24 @@ impl Miner {
             })?,
         );
 
+        // let transfer_tx_data = base64::prelude::BASE64_STANDARD.encode(
+        //     bincode::serialize(&transfer_tx).map_err(|e| {
+        //         ClientError::from(ClientErrorKind::Custom(format!(
+        //             "Bincode serialization error for transfer tx: {}",
+        //             e
+        //         )))
+        //     })?,
+        // );
+
         let body = json!({
-            "transaction": {
-                "content": tx_data
-            },
-            "skipPreFlight": skip_pre_flight,
-            "useStakedRPCs": use_staked_rpcs,
-            "tip": 1000000
+            "transactions": vec![tx_data, /* transfer_tx_data*/]
         });
 
         println!("auth token {}", auth_token);
-        println!("url {}", url);
         println!("body {}", body);
 
         let response: serde_json::Value = client
-            .post(url)
+            .post(URL)
             .json(&body)
             .header("Authorization", auth_token)
             .send()
